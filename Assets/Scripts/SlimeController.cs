3f45db7b221be54d691b2d6da60594719e0e48d1
gusdn6763 <gusdn6763@gmail.com>
2021-05-17 01:20:15 +0900

    Fix Merge

---
+++ Assets/Scripts/SlimeController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SlimeStatus
+{
+    Idle,
+    Attack,
+    Move,
+};
+
+public class SlimeController : MonoBehaviour
+{
+    private AudioSource audiod;
+    private Animator animator;
+    public Transform player;
+    public SlimeStatus slimeStatus = SlimeStatus.Idle;
+    //test
+    public float attackDist;
+    public float speed;
+    public bool isAttack = false;
+    public Coroutine slimeCoroutine;
+
+    private void Awake()
+    {
+        player = Player.instance.transform;
+        animator = GetComponent<Animator>();
+        audiod = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SlimeAction());
+        StartCoroutine(CheckState());
+    }
+
+    IEnumerator CheckState()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        while (true)
+        {
+            float dist = Vector3.Distance(player.position, transform.position);
+            if (dist <= attackDist)
+            {
+                slimeStatus = SlimeStatus.Attack;
+            }
+            else
+            {
+                slimeStatus = SlimeStatus.Move;
+            }
+            yield return (0.3f);
+        }
+    }
+
+    IEnumerator SlimeAction()
+    {
+        while(true)
+        {
+            transform.LookAt(Player.instance.transform);
+            switch (slimeStatus)
+            {
+                case SlimeStatus.Idle:
+                    break;
+
+                case SlimeStatus.Move:
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                    animator.SetBool("Move", true);
+                    isAttack = false;
+                    animator.SetBool("Attack", isAttack);
+                    break;
+                case SlimeStatus.Attack:
+                    animator.SetBool("Move", false);
+                    if (isAttack == false)
+                    {
+                        isAttack = true;
+                        animator.SetBool("Attack", isAttack);
+                    }
+                    break;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+    public void AttackSound()
+    {
+        audiod.Play();
+    }
+}
